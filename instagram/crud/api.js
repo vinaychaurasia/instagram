d4bde6497a95c73a19c7891528810634ab358249
vinaychaurasiya <contacctserver@gmail.com>
2020-09-07 17:06:44 +0530

    completed all the middleware (post, get, patch, delete)  of route user

--- instagram/crud/api.js
+++ instagram/crud/api.js
@@ -5,27 +5,22 @@
 // in pkg.json dir => npm start
 const express = require("express");
 const app = express();
-const userDB = require("./user.json");
+let userDB = require("./user.json");
 const fs = require("fs");
 const path = require("path");
 // REST API
 // HTTP request => 
-// create => POST
 // http packet => body 
 app.use(express.json());
 // handler req.body 
 app.post("/api/users", function (req, res) {
     let user = req.body;
-    // db Save
-    // console.log(user);
     // if a new entry is created on server
     // memory -> ram
     userDB.push(user);
-    fs.writeFileSync(path.join(__dirname, 
-        "user.json"),
-        JSON.stringify(userDB));
-    //    res status code server send 
-    res.status(201).json({
+    fs.writeFileSync(path.join(__dirname,"user.json"), JSON.stringify(userDB));
+    //    res status code server sends
+    return res.status(201).json({
         success: "successfull",
         user: user
     })
@@ -40,35 +35,74 @@ app.get("/api/users/:user_id", function (req, res) {
             user = userDB[i];
         }
     }
-    res.status(200).json({
+    // no user or unknown user req
+    if(user == undefined){
+        return res.status(404).json({
+            status : "failure",
+            "message" : "user not found"
+        })    
+    }
+    // authorised user
+    return res.status(200).json({
         status: "success",
-        user: user != undefined ? user : "no user"
+        "message" : "user found"
     })
 })
 // update => PATCH
-// client will your id in url and data to update in req.body
-// api/users/12345
 app.patch("/api/users/:user_id", function (req, res) {
     let { user_id } = req.params;
-    // {user_id:12345}
     let user;
-    let toUpdate=req.body;
+    //all updated req recieved in toUpdate
+    let toUpdate = req.body;
     for (let i = 0; i < userDB.length; i++) {
         if (userDB[i].user_id == user_id) {
             user = userDB[i];
         }
     }
-    // update 
-    res.status(200).json({
+    // unknown user
+    if(user == undefined){
+        return res.status(404).json({
+            status : "failure",
+            "message" : "user not found"
+        })    
+    }
+
+    //add if not present else update
+    for(let key in toUpdate){
+        user[key] = toUpdate[key];
+    }
+    
+    // write user in json file
+    fs.writeFileSync(path.join(__dirname,"user.json"), JSON.stringify(userDB));
+
+    return res.status(200).json({
         status: "success",
-        user: user != undefined ? user : "no user"
+        "message" : "user updated"
     })
 })
 // search and delete 
 app.delete("/api/users/:user_id", function (req, res) {
+    let {user_id} = req.params;
+    // intial length of DB
+    let intiLength = userDB.length;
+    userDB = userDB.filter(function(user){
+        return user.user_id != user_id;
+    })
+    //if intial len == final length that means user not found
+    if(intiLength == userDB.length){
+        return res.status(404).json({
+            status : "failure",
+            "message" : "user not found"    
+        })
+    }
+    // user found and deleted
+    fs.writeFileSync(path.join(__dirname, "user.json"), JSON.stringify(userDB));
+    return res.status(200).json({
+        status : "succesfull",
+        "message" : "user deleted"
+    })
 
 })
-// delete=> DELETE 
 // localhost:3000/api/users
 app.listen(3000, function () {
     console.log("Server is listening at port 3000");
